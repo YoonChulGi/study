e6f24a851fe65f01a65c1678a61f6a7f7b0b93de
YoonChulGi <dbscjfrl321@github.com>
2021-10-01 18:11:26 +0900

    interceptor 포스팅을 위한 소스 추가

---
+++ board2/src/main/java/board2/configuration/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package board2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import board2.interceptor.LoggerInterceptor;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer{
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor());
+	}
+}
