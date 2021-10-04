f71633da6b2b7c655156fc201a6abf78eb640360
YoonChulGi <dbscjfrl321@github.com>
2021-10-04 22:14:11 +0900

    파일다운로드 관련 포스팅하기위한 소스 추가

--- board2/src/main/java/board2/board/controller/BoardController.java
+++ board2/src/main/java/board2/board/controller/BoardController.java
@@ -1,17 +1,24 @@
 package board2.board.controller;
 
+import java.io.File;
+import java.net.URLEncoder;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import board2.board.dto.BoardDto;
+import board2.board.dto.BoardFileDto;
 import board2.board.service.BoardService;
 
 
@@ -67,4 +74,22 @@ public class BoardController {
 		boardService.deleteBoard(boardIdx);
 		return "redirect:/board/openBoardList.do";
 	} // 글이 수정 또는 삭제되면 게시글 목록 화면으로 이동합니다. 
+	
+	@RequestMapping("/board/downloadBoardFile.do")
+	public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception {
+		BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
+		if(ObjectUtils.isEmpty(boardFile) == false) {
+			String fileName = boardFile.getOriginalFileName();
+			byte[] files = FileUtils.readFileToByteArray(new File(boardFile.getStoredFilePath()));
+			
+			response.setContentType("application/octet-stream");
+			response.setContentLength(files.length);
+			response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName,"UTF-8")+ "\";");
+			response.setHeader("Content-Transfer-Encoding", "binary");
+			
+			response.getOutputStream().write(files);
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+		}
+	}
 }
