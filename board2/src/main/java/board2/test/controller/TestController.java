b1cec0cab5b870cad86992c1223e47c547d5a8cb
YoonChulGi <dbscjfrl321@github.com>
2021-10-29 00:01:07 +0900

    세션,쿠키 포스팅을 위한 소스추가

--- board2/src/main/java/board2/test/controller/TestController.java
+++ board2/src/main/java/board2/test/controller/TestController.java
@@ -1,29 +1,49 @@
 package board2.test.controller;
 
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import board2.test.service.TestService;
 
-@RestController
+@Controller
 public class TestController {
 	
 	@Autowired
 	TestService testService;
 	
 	@RequestMapping("/test/test.do")
-	List<Map<String,Object>> test(
-			@RequestParam(value="sort",defaultValue = "") String sort,
-			@RequestParam(value="department",defaultValue="") String department,
-			@RequestParam(value="publisher",defaultValue="") String publisher,
-			@RequestParam(value="age",defaultValue="") String age
-		) throws Exception{
+	ModelAndView test(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mv = new ModelAndView("/test/test");
 		
-		return testService.sendHighLevelApi("combook_*", sort, department, publisher, age);
+		// 쿠키 초기값 = 장바구니 정보
+		Cookie cookie = new Cookie("cartInfo","장바구니정보");
+		cookie.setMaxAge(60*60*24*365); // 1년
+		cookie.setPath("/"); // 모든 경로에서 접근 가능 하도록 설정
+		response.addCookie(cookie);
+		
+		
+		Cookie[] myCookies = request.getCookies();
+		for(Cookie c : myCookies) {
+			if("cartInfo".equals(c.getName())) {
+				c.setValue(null);
+				c.setMaxAge(0);
+				c.setPath("/");
+				response.addCookie(c);
+			}
+		}
+		
+		Cookie[] myCookies2 = request.getCookies();
+		for(Cookie c : myCookies2) {
+			if("cartInfo".equals(c.getName())) {
+				mv.addObject(c.getName(), c.getValue());
+			}
+		}
+		return mv;
 	}
 }
