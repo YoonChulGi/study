ea6ae7670023d057ea68c85cc07768f92f1abaee
YoonChulGi <dbscjfrl321@github.com>
2021-12-24 03:04:37 +0900

    React.js 하이어오더 컴포넌트 기초 공부 및 포스팅 중 소스 추가

--- example/src/05/05-1-compose.js
+++ example/src/05/05-1-compose.js
@@ -9,16 +9,11 @@ function compose_old(funcArr) {
     );
 }
 
-// const formulaWithCompose = compose_old([multiplyTwo, multiplyThree, addFour]);
-
-function compose() {
-    // Arrray의 prototype 객체 안에 정의된 slice() 함수를 사용하여 나열형 자료를 배열로 변환합니다. 
-    // 자바스크립트의 트릭으로 실무에서는 arguments를 배열로 변환할 때 자주 사용되는 패턴입니다. 
-    const funcArr = Array.prototype.slice.call(arguments);
+function compose(...funcArr) {    
     return funcArr.reduce(
         function (prevFunc, nextFunc) {
-            return function(value) {
-                return nextFunc(prevFunc(value));
+            return function(...args) {
+                return nextFunc(prevFunc(...args));
             }
         },
         function(k) { return k; }
@@ -26,3 +21,32 @@ function compose() {
 }
 
 const formulaWithCompose = compose(multiplyTwo, multiplyThree, addFour);
+// 혹은
+const formulaWithCompose = compose(
+    multiplyTwo,
+    multiplyThree,
+    addFour);
+const x = 10;
+formulaWithCompose(10); // 64
+
+// => ((x * 2) * 3) + 4
+// 동일한 함수 표현식
+const formulaWithCompose = compose(
+    multiplyX(2),
+    multiplyX(3),
+    addX(4)
+);
+
+// 조합 함수 없이 표현한 경우
+const formulaWithoutCompose = addX(4)(multiplyX(3)(multiplyX(2)));
+
+// ((x * 2) + 5 ) * 3) + 4 연산이 추가된 공식
+const formulaWithCompose2 = compose(
+    multiplyX(2),
+    addX(5),
+    multiplyX(3),
+    addX(4)
+);
+
+// 조합 함수 없이 표현한 경우
+const formulaWithoutCompose2 = addX(4)(multiplyX(3)(addX(5)(multiplyX(2))));
---
+++ example/src/05/withHoC.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function withHoC(WrappedComponent) {
+  return class WithHoc extends React.Component {
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+}
