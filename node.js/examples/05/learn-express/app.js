32ef175c35e7d019a87058130d160133aa8267d5
YoonChulGi <dbscjfrl321@github.com>
2022-01-30 02:49:15 +0900

    Node.js 템플릿 엔진 사용하기 공부 및 포스팅 완료 관련 소스추가

--- node.js/examples/05/learn-express/app.js
+++ node.js/examples/05/learn-express/app.js
@@ -40,7 +40,16 @@ app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 const multer = require('multer');
