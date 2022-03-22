a014028239346aa66dbeee9fe3281dc281136283
윤철기 <31471631+YoonChulGi@users.noreply.github.com>
2022-03-23 01:18:51 +0900

    update loginLogSelectors.js

--- spb-admin/src/selectors/loginLogSelectors.js
+++ spb-admin/src/selectors/loginLogSelectors.js
@@ -1,6 +1,7 @@
 import createSelectors from "../api-redux-pack/createSelectors";
 
 export const {
+  // test
   loginLogSelector: loginLogListSelector,
   loginLogLoadingStateSelector: loginLogListLoadingStateSelector,
   paginationSelector,
