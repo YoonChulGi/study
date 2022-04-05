03c93d1eaff90fe747f66c2e6470e8f13e6d469c
윤철기 <31471631+YoonChulGi@users.noreply.github.com>
2022-04-05 22:34:34 +0900

    10.4 수동으로 톰켓에 배포하기 - cloud-config 404 해결하기        - 해결. Tomcat 9.0으로 업데이트하여 web4.0 warning 해결,        - CloudConfigApplication 에 SpringBootServletInitializer를 extends 하여 web.xml이 없어서 tomcat호출시 404 에러 해결       - gradle에서 java version 1.8로 downgrade 하여 tomcat의 java version과 맞춤
    TODO)
        포스팅및 spb 구동을 위한 포트포워딩 (이중 공유기 아빠 없을때 거실 공유기 뒷면 확인하여 비밀번호 알아내기)

--- cloud-config/src/main/java/cloudconfig/CloudConfigApplication.java
+++ cloud-config/src/main/java/cloudconfig/CloudConfigApplication.java
@@ -2,12 +2,19 @@ package cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class CloudConfigApplication {
-
+public class CloudConfigApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CloudConfigApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
 	}
--- spb/src/main/java/spb/SpbApplication.java
+++ spb/src/main/java/spb/SpbApplication.java
@@ -3,7 +3,9 @@ package spb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -13,7 +15,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		basePackageClasses = {Jsr310JpaConverters.class},
 		basePackages = {"spb"})
 @SpringBootApplication
-public class SpbApplication {
+public class SpbApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpbApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpbApplication.class, args);
